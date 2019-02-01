46746aa5f56ae815026b71023953235367b7223e
Siddhesh Rane <siddheshrane24@gmail.com>
2019-02-01 13:27:19 +0530

    adds qrgenerator

--- app/src/main/java/com/example/sid24rane/blockcanteen/KeyGenerationActivity.java
+++ app/src/main/java/com/example/sid24rane/blockcanteen/KeyGenerationActivity.java
@@ -1,13 +1,16 @@
 package com.example.sid24rane.blockcanteen;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+//https://github.com/scottyab/secure-preferences ==> can be used to store values securely
 public class KeyGenerationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_key_generation);
+
+
     }
 }
