1039118f8e088448b0241271c0f3c28c333ac059
Yash Jain <yashjain864@gmail.com>
2019-02-01 16:26:29 +0530

    User Model Updated!

--- app/src/main/java/com/example/sid24rane/blockcanteen/KeyGeneration/UserModel.java
+++ app/src/main/java/com/example/sid24rane/blockcanteen/KeyGeneration/UserModel.java
@@ -1,11 +1,16 @@
 package com.example.sid24rane.blockcanteen.KeyGeneration;
 
 public class UserModel {
+
+        private String id;
         private String firstName;
         private String lastName;
         private String email;
-        private String id;
         private String publicKey;
+        private String department;
+        private String entryYear;
+        private String userType; //Student or faculty
+
 
         public UserModel(String firstName, String lastName, String email, String id, String publicKey){
             this.email = email;
@@ -15,24 +20,36 @@ public class UserModel {
             this.publicKey = publicKey;
         }
 
-    public String getFirstName() {
-        return firstName;
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getPublicKey() {
+            return publicKey;
+        }
+
+        public String getLastName(){
+                return lastName;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+    public String getDepartment() {
+        return department;
     }
 
-    public String getEmail() {
-        return email;
+    public String getEntryYear() {
+        return entryYear;
     }
 
-    public String getPublicKey() {
-        return publicKey;
-    }
-
-    public String getLastName(){
-            return lastName;
-    }
-
-    public String getId() {
-        return id;
+    public String getUserType() {
+        return userType;
     }
 
 }
