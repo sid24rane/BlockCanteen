0fed7216f7c64272270aabf0fc1de16787eb6501
Siddhesh Rane <siddheshrane24@gmail.com>
2019-02-13 20:25:29 +0530

    adds konfetti

--- app/src/main/java/com/example/sid24rane/blockcanteen/SendActivity.java
+++ app/src/main/java/com/example/sid24rane/blockcanteen/SendActivity.java
@@ -1,6 +1,7 @@
 package com.example.sid24rane.blockcanteen;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,8 +9,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.sid24rane.blockcanteen.Dashboard.DashboardActivity;
 import com.example.sid24rane.blockcanteen.data.KeyInSharedPreferences;
 import com.example.sid24rane.blockcanteen.utilities.TransactionUtils;
+
+import nl.dionsegijn.konfetti.KonfettiView;
+import nl.dionsegijn.konfetti.models.Shape;
+import nl.dionsegijn.konfetti.models.Size;
 
 public class SendActivity extends AppCompatActivity {
 
@@ -18,6 +24,7 @@ public class SendActivity extends AppCompatActivity {
     private TextView email;
     private String receiverPublicKey;
     private EditText amount;
+    private KonfettiView viewKonfetti;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +46,7 @@ public class SendActivity extends AppCompatActivity {
         name = (TextView) findViewById(R.id.username);
         email = (TextView) findViewById(R.id.email);
         amount = (EditText) findViewById(R.id.amount);
+        viewKonfetti = (KonfettiView) findViewById(R.id.viewKonfetti);
 
         send.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,7 +54,23 @@ public class SendActivity extends AppCompatActivity {
 
                 //String sender_pub_key = "MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAExcIsvLH3vegArqtP7wEdyly11xAcrpV4IBIUCVM+HXoPMMpNFX8hYDjOPL4IUT4swqDkrhj1gS+XWukiGpttzQ==";
                 String publicKey = KeyInSharedPreferences.retrievingPublicKey(SendActivity.this);
+
                 TransactionUtils.makeTransaction(amount.getText().toString(),publicKey,receiverPublicKey, SendActivity.this);
+
+                viewKonfetti.build()
+                        .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                        .setDirection(0.0, 359.0)
+                        .setSpeed(1f, 5f)
+                        .setFadeOutEnabled(true)
+                        .setTimeToLive(2000L)
+                        .addShapes(Shape.RECT, Shape.CIRCLE)
+                        .addSizes(new Size(12, 5))
+                        .setPosition(-50f, viewKonfetti.getWidth() + 50f, -50f, -50f)
+                        .streamFor(300, 5000L);
+
+                Intent intent = new Intent(SendActivity.this,DashboardActivity.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.slide_in_left,android.R.anim.slide_out_right);
             }
         });
     }
